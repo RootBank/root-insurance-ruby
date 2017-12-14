006e3bfcb260140ce4ef91cde0ff497a26e466cc
Martin Pretorius <glasnoster@gmail.com>
2017-12-14 12:05:11 +0200

    Quote api also works for term and funeral insurance

--- lib/root/insurance/client/quote.rb
+++ lib/root/insurance/client/quote.rb
@@ -1,16 +1,59 @@
 class Root::Insurance::Client
   module Quote
-    def create_quote(type:, model_name: nil)
+    def create_quote(opts={})
+      type = opts[:type]
+
+      case type.to_sym
+      when :root_gadgets
+        create_gadget_quote(opts)
+      when :root_term
+        create_term_quote(opts)
+      when :root_funeral
+        create_funeral_quote(opts)
+      else
+        raise ArgumentError("Unknown quote type: #{type}")
+      end
+    end
+
+    def list_gadget_models
+      get('gadgets/models')
+    end
+
+    private
+    def create_gadget_quote(opts)
       data = {
-        type:       type,
-        model_name: model_name
+        type:       :root_gadgets,
+        model_name: opts[:model_name]
       }
 
       post(:quotes, data)
     end
 
-    def list_gadget_models
-      get('gadgets/models')
+    def create_term_quote(opts)
+      data = {
+        type:             :root_term,
+        cover_amount:     opts[:cover_amount],
+        cover_period:     opts[:cover_period],
+        education_status: opts[:education_status],
+        smoker:           opts[:smoker],
+        gender:           opts[:gender],
+        age:              opts[:age],
+        basic_income_per_month: opts[:basic_income_per_month],
+      }
+
+      post(:quotes, data)
+    end
+
+    def create_funeral_quote(opts)
+      data = {
+        type:                 :root_funeral,
+        cover_amount:         opts[:cover_amount],
+        has_spouse:           opts[:has_spouse],
+        number_of_children:   opts[:number_of_children],
+        extended_family_ages: opts[:extended_family_ages]
+      }
+
+      post(:quotes, data)
     end
   end
 end
--- spec/root/insurance/client/quote_spec.rb
+++ spec/root/insurance/client/quote_spec.rb
@@ -9,17 +9,7 @@ describe Root::Insurance::Client::Quote do
   let(:client) { Root::Insurance::Client.new(app_id, app_id, environment) }
 
   describe :create_quote do
-    context "without a model name" do
-      it "posts to the correct data to the correct url" do
-        stub_request(:post, url)
-          .with(body: {type: "root_gadgets"})
-          .to_return(body: "{}")
-
-        client.create_quote(type: "root_gadgets")
-      end
-    end
-
-    context "with a model name" do
+    context "root gadgets" do
       let(:type) { "root_gadgets" }
       let(:model_name) { "iPhone 6s 64GB LTE" }
 
@@ -31,6 +21,78 @@ describe Root::Insurance::Client::Quote do
         client.create_quote(type: type, model_name: model_name)
       end
     end
+
+    context "term cover" do |variable|
+      let(:type) { "root_term" }
+      let(:cover_amount) { 100_000_00 }
+      let(:cover_period) { 'one_year' }
+      let(:basic_income_per_month) { 20_000_00 }
+      let(:education_status) { 'grade_12_matric' }
+      let(:smoker) { true }
+      let(:gender) { 'male' }
+      let(:age) { 31 }
+
+      let(:expected_data) do
+        {
+          type: type,
+          cover_amount: cover_amount,
+          cover_period: cover_period,
+          basic_income_per_month: basic_income_per_month,
+          education_status: education_status,
+          smoker: smoker,
+          gender: gender,
+          age:    age
+        }
+      end
+
+      it "posts to the correct data to the correct url" do
+        stub_request(:post, url)
+          .with(body: expected_data)
+          .to_return(body: "{}")
+
+        client.create_quote(
+          type: type,
+          cover_amount: cover_amount,
+          cover_period: cover_period,
+          basic_income_per_month: basic_income_per_month,
+          education_status: education_status,
+          smoker: smoker,
+          gender: gender,
+          age:    age)
+      end
+    end
+
+    context "funeral cover" do
+      let(:type) { "root_funeral" }
+      let(:cover_amount) { 10_000_00 }
+      let(:has_spouse) { true }
+      let(:number_of_children) { 2 }
+      let(:extended_family_ages) { [30, 21] }
+
+      let(:expected_data) do
+        {
+          type: type,
+          cover_amount:         cover_amount,
+          has_spouse:           has_spouse,
+          number_of_children:   number_of_children,
+          extended_family_ages: extended_family_ages
+        }
+      end
+
+       it "posts to the correct data to the correct url" do
+        stub_request(:post, url)
+          .with(body: expected_data)
+          .to_return(body: "{}")
+
+        client.create_quote(
+          type: type,
+          cover_amount:         cover_amount,
+          has_spouse:           has_spouse,
+          number_of_children:   number_of_children,
+          extended_family_ages: extended_family_ages)
+      end
+    end
+
   end
 
   describe :list_gadget_models do
